6d84af2faff74e1429a836ea409a2deab2cd4d25
RubyMyDear <kmlkamilek@gmail.com>
2019-11-28 01:09:40 +0100

    WIP: Load and store instructions #5

---
+++ tests/LoadInstructionsTests.cpp
@@ -0,0 +1,34 @@
+#include <memory>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "../src/CpuImpl.hpp"
+#include "../src/Memory.hpp"
+
+namespace
+{
+    using ::testing::Return;
+
+    class MemoryMock : public Memory
+    {
+    public:
+        MOCK_CONST_METHOD1(readByte, u8(u16));
+        MOCK_METHOD2(writeByte, void(u16, u8));
+        MOCK_CONST_METHOD1(readWord, u16(u16));
+        MOCK_METHOD2(writeWord, void(u16, u16));
+        MOCK_CONST_METHOD1(readControllerState, ControllerState(unsigned));
+    };
+
+    class LoadInstructionsTests : public ::testing::Test
+    {
+    protected:
+        void SetUp() override
+        {
+            memory = std::make_shared<MemoryMock>();
+            testedCpu = std::make_unique<CpuImpl>(memory);
+        }
+
+        std::unique_ptr<CpuImpl> testedCpu;
+        std::shared_ptr<MemoryMock> memory;
+    };
+};
