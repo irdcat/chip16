e865fcd90a5623248cfc7dd87a0d6b5f7250f80e
RubyMyDear <kmlkamilek@gmail.com>
2019-12-05 20:20:39 +0100

    Added some documentation to code

--- src/Cpu.hpp
+++ src/Cpu.hpp
@@ -7,11 +7,31 @@ class Cpu
 public:
 	virtual ~Cpu() = default;
 
+    /**
+     * Fetches opcode from memory.
+     *
+     * @return Fetched opcode.
+     */
     virtual u16 fetchOpcode() = 0;
 
+    /**
+     * Pops value from stack.
+     *
+     * @return Popped value.
+     */
     virtual u16 popFromStack() = 0;
 
+    /**
+     * Pushes value into stack.
+     *
+     * @param value Value to be pushed
+     */
     virtual void pushIntoStack(u16 value) = 0;
 
+    /**
+     * Executes instruction with given opcode.
+     *
+     * @param opcode Opcode of the instruction to be executed.
+     */
 	virtual void executeInstruction(u16 opcode) = 0;
 };
--- src/Memory.hpp
+++ src/Memory.hpp
@@ -8,13 +8,43 @@ class Memory
 public:
     virtual ~Memory() = default;
 
+    /**
+     * Reads byte from memory at given address.
+     *
+     * @param addr Address of the memory to read value from.
+     * @return Byte from memory.
+     */
     virtual u8 readByte(u16 addr) const = 0;
 
+    /**
+     * Writes byte into memory at given address.
+     * 
+     * @param addr Address of the memory to read value from.
+     * @param byte Byte to be written in memory.
+     */
     virtual void writeByte(u16 addr, u8 byte) = 0;
 
+    /**
+     * Reads word from memory at given address.
+     *
+     * @param addr Address of the memory to read value from.
+     * @return Word from memory.
+     */
     virtual u16 readWord(u16 addr) const = 0;
 
+    /**
+     * Writes word into memory at given address.
+     *
+     * @param addr Address of the memory to read value from.
+     * @param word Word to be written in memory.
+     */
     virtual void writeWord(u16 addr, u16 word) = 0;
 
+    /**
+     * Reads controlles state from memory.
+     *
+     * @param index Index of the controller.
+     * @return Controller state.
+     */
     virtual ControllerState readControllerState(unsigned index) const = 0;
 };
