bf26ad8f3b97acaf66c7976c71142d581360adc3
Kamil Adamiec <kmlkamilek@gmail.com>
2019-12-06 23:57:10 +0100

    Make log OutputModificator polymorfic instead of using template to indicate type #36 (#39)

---
+++ src/log/HexModificator.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <memory>
+#include <iomanip>
+#include <type_traits>
+#include "LogModificator.hpp"
+
+template<typename T>
+class HexModificator : public LogModificator<T>
+{
+public:
+    static_assert(std::is_integral_v<T>, "Type of T must be integral type!");
+
+    HexModificator(T value);
+
+    ~HexModificator() = default;
+
+    void apply(std::ostream& os) const override;
+};
+
+template<typename T>
+inline HexModificator<T>::HexModificator(T value)
+    : LogModificator(value)
+{
+}
+
+template<typename T>
+inline void HexModificator<T>::apply(std::ostream& os) const
+{
+    os << std::uppercase << std::hex << value;
+}
+
+template<typename T>
+inline std::shared_ptr<LogModificator<T>> logHex(T value)
+{
+    return std::make_shared<HexModificator<T>>(value);
+}
